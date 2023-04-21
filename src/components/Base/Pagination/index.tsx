bb4769a91e52359f604b1ac0371d851e83e19a33
‘Joex13’ <aokijo0113p@gmail.com>
2023-04-21 18:52:46 +0900

    ページネーション無理

--- src/components/Base/Pagination/index.tsx
+++ src/components/Base/Pagination/index.tsx
@@ -1,43 +1,14 @@
 import React from 'react';
-import Link from 'next/link';
 
 type Props = {
-  totalCount: number;
-  PER_PAGE: number;
-  currentPage: number;
+  children: React.ReactNode;
 };
 
-const Pagination = ({ totalCount, PER_PAGE, currentPage }: Props) => {
-  const range = (start: number, end: number) =>
-    [...Array(end - start + 1)].map((_, i) => start + i);
+const Pagination = ({ children }: Props) => {
 
   return (
     <nav className="mx-auto mt-4">
-      <ul className="flex justify-center gap-4">
-        {range(1, Math.ceil(totalCount / PER_PAGE)).map(
-          (number: number, index: number) => (
-            <li
-              key={index}
-              className={
-                'duration-300 ' +
-                (currentPage === number
-                  ? 'bg-orange-950 text-white'
-                  : 'bg-orange-600 hover:opacity-70')
-              }
-            >
-              <Link
-                className={
-                  'inline-block p-3 ' +
-                  (currentPage === number && 'pointer-events-none')
-                }
-                href={`/blog/${number}`}
-              >
-                {number}
-              </Link>
-            </li>
-          )
-        )}
-      </ul>
+      <ul className="flex justify-center gap-4">{children}</ul>
     </nav>
   );
 };
--- src/components/Feature/BlogPagination/index.tsx
+++ src/components/Feature/BlogPagination/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { Pagination } from '@/components/Base';
 
 type Props = {
@@ -8,14 +9,70 @@ type Props = {
 };
 
 const BlogPagination = ({ totalCount, PER_PAGE, currentPage }: Props) => {
+  const totalPagination = Math.ceil(totalCount / PER_PAGE);
+  const paginationStart = Math.max(1, currentPage - 2);
+  const paginationLast = Math.min(paginationStart + 4, totalPagination);
+  const range = () => {
+    if (currentPage >= paginationLast - 1) {
+      return Array.from({ length: 5 }, (_, i) => paginationLast - 4 + i);
+    }
+    return Array.from(
+      { length: paginationLast - paginationStart + 1 },
+      (_, i) => paginationStart + i
+    );
+  };
+
   return (
-    <>
-      <Pagination
-        totalCount={totalCount}
-        PER_PAGE={PER_PAGE}
-        currentPage={currentPage}
-      />
-    </>
+    <Pagination>
+      {currentPage >= (PER_PAGE + 2) ? (
+        <>
+          <li className="duration-300 bg-orange-600 hover:opacity-70">
+            <Link className={'inline-block p-3 '} href={`/blog/1`}>
+              1
+            </Link>
+          </li>
+          <span>...</span>
+        </>
+      ) : (
+        ''
+      )}
+      {range().map((number: number, index: number) => (
+        <li
+          key={index}
+          className={
+            'duration-300 ' +
+            (currentPage === number
+              ? 'bg-orange-950 text-white'
+              : 'bg-orange-600 hover:opacity-70')
+          }
+        >
+          <Link
+            className={
+              'inline-block p-3 ' +
+              (currentPage === number && 'pointer-events-none')
+            }
+            href={`/blog/${number}`}
+          >
+            {number}
+          </Link>
+        </li>
+      ))}
+      {currentPage <= totalPagination - (PER_PAGE + 1) ? (
+        <>
+          <span>...</span>
+          <li className="duration-300 bg-orange-600 hover:opacity-70">
+            <Link
+              className={'inline-block p-3 '}
+              href={`/blog/${totalPagination}`}
+            >
+              7
+            </Link>
+          </li>
+        </>
+      ) : (
+        ''
+      )}
+    </Pagination>
   );
 };
 

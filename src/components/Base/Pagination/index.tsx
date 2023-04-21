49c9281d765b2a837469f80a2272da5b465993c8
‘Joex13’ <aokijo0113p@gmail.com>
2023-04-21 15:20:21 +0900

    ディレクトリ整理

--- src/components/Base/Pagination/index.tsx
+++ src/components/Base/Pagination/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import Link from 'next/link';
 
 type Props = {
-  children: React.ReactNode;
+  totalCount: number;
+  PER_PAGE: number;
+  currentPage: number;
 };
 
-const Pagination = ({ children }: Props) => {
+const Pagination = ({ totalCount, PER_PAGE, currentPage }: Props) => {
+  const range = (start: number, end: number) =>
+    [...Array(end - start + 1)].map((_, i) => start + i);
+
   return (
     <nav className="mx-auto mt-4">
-      <ul className="flex justify-center gap-4">{children}</ul>
+      <ul className="flex justify-center gap-4">
+        {range(1, Math.ceil(totalCount / PER_PAGE)).map(
+          (number: number, index: number) => (
+            <li
+              key={index}
+              className={
+                'duration-300 ' +
+                (currentPage === number
+                  ? 'bg-orange-950 text-white'
+                  : 'bg-orange-600 hover:opacity-70')
+              }
+            >
+              <Link
+                className={
+                  'inline-block p-3 ' +
+                  (currentPage === number && 'pointer-events-none')
+                }
+                href={`/blog/${number}`}
+              >
+                {number}
+              </Link>
+            </li>
+          )
+        )}
+      </ul>
     </nav>
   );
 };
--- src/components/Feature/BlogPagination/index.tsx
+++ src/components/Feature/BlogPagination/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Pagination } from '@/components/Base';
-import Link from 'next/link';
 
 type Props = {
   totalCount: number;
@@ -9,33 +8,14 @@ type Props = {
 };
 
 const BlogPagination = ({ totalCount, PER_PAGE, currentPage }: Props) => {
-  const range = (start: number, end: number) =>
-    [...Array(end - start + 1)].map((_, i) => start + i);
-
   return (
-    <Pagination>
-      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
-        <li
-          key={index}
-          className={
-            'duration-300 ' +
-            (currentPage === number
-              ? 'bg-orange-950 text-white'
-              : 'bg-orange-600 hover:opacity-70')
-          }
-        >
-          <Link
-            className={
-              'inline-block p-3 ' +
-              (currentPage === number && 'pointer-events-none')
-            }
-            href={`/blog/${number}`}
-          >
-            {number}
-          </Link>
-        </li>
-      ))}
-    </Pagination>
+    <>
+      <Pagination
+        totalCount={totalCount}
+        PER_PAGE={PER_PAGE}
+        currentPage={currentPage}
+      />
+    </>
   );
 };
 
